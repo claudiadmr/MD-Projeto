ff52ad875d52c1f326aca3ec834aa20cc13730ad
claudiadmr <claudiadm.ribeiro@hotmail.com>
2023-06-15 20:54:45 +0100

    update

--- Fase3/application/app.py
+++ Fase3/application/app.py
@@ -1,15 +1,54 @@
 from flask import Flask, request, render_template, redirect
-
 import webscraping
 
 app = Flask(__name__)
 
+# Sample data
+data = {
+    "Price": {
+        "Name": "Price",
+        "Positive Reviews": ["Great for the price", "Excellent value for the mone Excellent value for the money Excellent value for the money Excellent value for the money Excellent value for the money Excellent value for the moneyy", "Affordable and high quality"],
+        "Negative Reviews": ["Cheaply made", "You get what you pay forYou get what you pay forYou get what you pay forYou get what you pay forYou get what you pay for", "You get what you pay for", "You get what you pay for", "You get what you pay for", "You get what you pay for", "You get what you pay for"]
+    },
+    "Durability": {
+        "Name": "Durability",
+        "Positive Reviews": ["Built to last", "Sturdy construction", "Handles wear and tear well"],
+        "Negative Reviews": ["Broke easily", "Not very sturdy", "Poor quality materials"]
+    },
+    "Durabilitys": {
+        "Name": "Durabilitys",
+        "Positive Reviews": ["Built to last", "Sturdy construction", "Handles wear and tear well"],
+        "Negative Reviews": ["Broke easily", "Not very sturdy", "Poor quality materials"]
+    },
+    "Durabilityss": {
+        "Name": "Durabilityss",
+        "Positive Reviews": ["Built to last", "Sturdy construction", "Handles wear and tear well"],
+        "Negative Reviews": ["Broke easily", "Not very sturdy", "Poor quality materials"]
+    },
+    "Durabilitysss": {
+        "Name": "Durabilitysss",
+        "Positive Reviews": ["Built to last", "Sturdy construction", "Handles wear and tear well"],
+        "Negative Reviews": ["Broke easily", "Not very sturdy", "Poor quality materials"]
+    }
+}
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
 @app.route('/analyze_reviews', methods=['GET'])
 def analyze_reviews():
     if request.method == 'GET':
-        amazon = request.args.get('amazon')
-        walmart = request.args.get('walmart')
-        return webscraping.run_scraper(amazon, walmart)
+        amazon = request.args.get("product_id_amazon")
+        walmart = request.args.get("product_id_walmart")
+       # datat = webscraping.openaiRequest()
+        #print(datat)
+
+        # return webscraping.openaiRequest()
+        return render_template('reviews.html', data=data)
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
--- Fase3/application/webscraping.py
+++ Fase3/application/webscraping.py
@@ -1,10 +1,19 @@
 import requests
 import threading
 from flask import jsonify
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.schema import Document
+import os
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
 
 class APIRequestThread(threading.Thread):
     def __init__(self, url):
@@ -43,3 +52,53 @@ def run_scraper(amazon, walmart):
     print(len(combined_data))
     return jsonify(combined_data)
 
+
+def joinScrapyData():
+    import json
+    with open("static/response.json") as json_file:
+        data = json.load(json_file)
+        # data['data1'] = data['data1']['items']
+        # data['data2'] = data['data2']['items']
+        # data = data['data1'] + data['data2']
+        print(len(data))
+    return data
+
+
+def openaiRequest():
+    data = joinScrapyData()
+    documents = []
+    for item in data:
+        metadata = {'rating': item['rating']}
+        if item['title'] is not None:
+            metadata['title'] = item['title']
+        if item['product'] is not None:
+            metadata['product'] = item['product']
+
+        document = Document(page_content=item['text'], metadata=metadata)
+        documents.append(document)
+
+    # Split the text in chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+    # Create a persistent, file-based vector store
+    directory = 'index_store'
+    vector_index = Chroma.from_documents(documents, OpenAIEmbeddings(), persist_directory=directory)
+    vector_index.persist()
+
+    # Create the retriever and the query-interface
+    retriever = vector_index.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+    qa_interface = RetrievalQA.from_chain_type(llm=ChatOpenAI(), chain_type="stuff", retriever=retriever,
+                                               return_source_documents=True)
+
+    # Query GPT-3
+    response = qa_interface("""Please analyze the following collection of reviews and extract the relevant features for the product. 
+    For each feature, provide a list of positive and negative comments. Response format: 
+    featureName: 
+    -name: x 
+    -Positive Feature Reviews: (only the ones about this feature) 
+    -Negative Feature Reviews: (only the ones about this feature) 
+    If there are no positive or negative characteristics, write “Not applicable”. 
+    Provide it in JSON format.""")
+    print(response)
+    print(response['result'])
+    return response['result']
